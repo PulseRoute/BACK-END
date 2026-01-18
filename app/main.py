f0117da40fa89c9cb60d5fdb0dfe82b38cf96c23
devksy <star@devksy.xyz>
2026-01-19 02:01:28 +0900

    feat: CORS 미들웨어에 pulseroute.vercel.app 추가

--- app/main.py
+++ app/main.py
@@ -30,6 +30,7 @@ app.add_middleware(
         "http://127.0.0.1:3000",
         "http://127.0.0.1:8000",
         "http://127.0.0.1:8001",
+        "https://pulseroute.vercel.app"
         "*",
     ],
     allow_credentials=True,
